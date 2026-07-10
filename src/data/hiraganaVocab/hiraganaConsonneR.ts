7300574796b0e482eed8dcc70d2e975b884ee066
Yohann Faustino <yohannfaustino@gamil.com>
2026-07-10 20:21:28 +0200

    correction hiragana consonne R

--- src/data/hiraganaVocab/hiraganaConsonneR.ts
+++ src/data/hiraganaVocab/hiraganaConsonneR.ts
@@ -1,7 +1,7 @@
 // VOCABULAIRE (LES MÊMES 15 MOTS QUE DANS LES QUESTIONS 1-15)
 export const hiraganaRVocab = [
-  { jp: "らいおん", romaji: "raion", fr: "lion" },
-  { jp: "りん", romaji: "rin", fr: "anneau / sonnette" },
+  { jp: "らいお", romaji: "raio", fr: "lion (abrégé)" },
+  { jp: "りこ", romaji: "riko", fr: "raison / logique" },
   { jp: "るす", romaji: "rusu", fr: "absence" },
   { jp: "れい", romaji: "rei", fr: "salutation / zéro" },
   { jp: "ろうそく", romaji: "rousoku", fr: "bougie" },
@@ -10,9 +10,9 @@ export const hiraganaRVocab = [
   { jp: "るり", romaji: "ruri", fr: "lapis-lazuli (pierre)" },
   { jp: "れきし", romaji: "rekishi", fr: "histoire" },
   { jp: "ろく", romaji: "roku", fr: "six" },
-  { jp: "らん", romaji: "ran", fr: "orchidée / désordre" },
+  { jp: "らき", romaji: "raki", fr: "chance" },
   { jp: "りす", romaji: "risu", fr: "écureuil" },
   { jp: "るい", romaji: "rui", fr: "catégorie / type" },
   { jp: "れつ", romaji: "retsu", fr: "rangée / file" },
-  { jp: "ろてん", romaji: "roten", fr: "en plein air" },
+  { jp: "ろて", romaji: "rote", fr: "routine" },
 ];
