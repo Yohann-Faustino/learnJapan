01e737dc0cb07f3edefd01598bf4fb54d1065913
Yohann Faustino <yohannfaustino@gmail.com>
2026-08-18 19:46:23 +0200

    tentative fixe bug deploiement

--- src/components/ThemeToogle.tsx
+++ src/components/ThemeToogle.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "../context/themeContext";
+import { useTheme } from "../context/ThemeContext";
 import styles from "./ThemeToogle.module.css";
 
 export default function ThemeToggle() {
